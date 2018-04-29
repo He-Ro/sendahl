13dc9dc2a3537762e06d64d27fc6aba9f640b2c6
H. Rosendahl <h@ro.sendahl.de>
2018-04-29 15:07:38 +0200

    Add initial Pelican development files

---
+++ pelicanconf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+""" My Pelican configuration """
+from __future__ import unicode_literals
+
+AUTHOR = 'Hendrik'
+SITENAME = 'ro.sendahl'
+SITEURL = 'http://ro.sendahl.de'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+THEME = 'themes/blue-penguin'
+
+DEFAULT_PAGINATION = False
+
+# provided as examples, they make ‘clean’ urls. used by MENU_INTERNAL_PAGES.
+TAGS_URL = 'tags'
+TAGS_SAVE_AS = 'tags/index.html'
+AUTHORS_URL = 'authors'
+AUTHORS_SAVE_AS = 'authors/index.html'
+CATEGORIES_URL = 'categories'
+CATEGORIES_SAVE_AS = 'categories/index.html'
+ARCHIVES_URL = 'archives'
+ARCHIVES_SAVE_AS = 'archives/index.html'
+
+# use those if you want pelican standard pages to appear in your menu
+MENU_INTERNAL_PAGES = (
+    # ('Tags', TAGS_URL, TAGS_SAVE_AS),
+    # ('Authors', AUTHORS_URL, AUTHORS_SAVE_AS),
+    ('Categories', CATEGORIES_URL, CATEGORIES_SAVE_AS),
+    ('Archives', ARCHIVES_URL, ARCHIVES_SAVE_AS),
+)
+# additional menu items
+MENUITEMS = (
+    ('Mail', 'https://webmail.strato.com/appsuite/signin'),
+)
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
---
+++ template.md
@@ -0,0 +1,10 @@
+Title: My title
+Date: 2018-04-29 10:20
+Modified: 2018-04-29 19:30
+Category: misc
+Tags: pelican, publishing
+Slug: my-title
+Authors: Hendrik Rosendahl
+Summary: Short version for index and feeds
+
+This is the content of my super blog post.
