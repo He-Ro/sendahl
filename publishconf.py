bd355c97a4031c955ae87eaaa3fd35e6d1731df6
H. Rosendahl <h@ro.sendahl.de>
2018-04-29 16:58:24 +0200

    Revert temporary changes to publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -13,9 +13,9 @@ from pelicanconf import *
 SITEURL = 'http://ro.sendahl.de'
 RELATIVE_URLS = False
 
-# FEED_ALL_ATOM = 'feeds/all.atom.xml'
-# CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
-# 
-# DELETE_OUTPUT_DIRECTORY = True
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+
+DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
